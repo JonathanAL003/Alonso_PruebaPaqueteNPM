a3e3374fbc912e447b68c0bf654dbb8b9cf0af7e
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2023-08-30 08:49:20 -0500

    Modulo NPM alpaq V 1.0.2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let frases=[
-    "Frase 1",
-    "Frase 2",
-    "Frase 3"
+    "El éxito es la suma de pequeños esfuerzos repetidos día tras día. - Robert Collier",
+    "El único modo de hacer un gran trabajo es amar lo que haces. - Steve Jobs",
+    "El camino hacia el éxito yace en no temerle al fracaso. - Winston Churchill",
+    "El fracaso es una oportunidad para empezar de nuevo, pero de forma más inteligente. - Henry Ford",
+    "No importa lo lento que vayas, siempre y cuando no te detengas. - Confucio"
 ]
 
 
