e510deb758be3fba40bd8deb2f65c0c96acaeb0f
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2023-10-10 08:46:45 -0500

    Comentarios con JsDocs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * Frases que se pueden obtener de manera random
+ */
 let frases=[
     "El éxito es la suma de pequeños esfuerzos repetidos día tras día. - Robert Collier",
     "El único modo de hacer un gran trabajo es amar lo que haces. - Steve Jobs",
@@ -6,11 +9,20 @@ let frases=[
     "No importa lo lento que vayas, siempre y cuando no te detengas. - Confucio"
 ]
 
-
+/**
+ * Esta funcion es la encargada de regresar una frase. 
+ * @param {Number} indice Se requiere de este numero para poder calcular de manera random, la frase que se regresera.
+ * @returns 
+ */
 function obtenerFrase(indice){
     return frases(indice);
 }
 
+/**
+ * Export del Modulo
+ */
 module.exports.obtenerFrase=obtenerFrase;
 
+
+/**Confirmacion de funcionamento en consola. */
 console.log(module);
